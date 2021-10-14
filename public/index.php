d1fef64bf109996db9e328140a013a1ff3543b3b
cordeiro <mjcc@tutanota.com>
2021-10-14 12:45:54 +0200

    add dotenv

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Router;
+use Symfony\Component\Dotenv\Dotenv;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 require '../vendor/autoload.php';
 
-$loader = new FilesystemLoader('../templates');
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+
+$loader = new FilesystemLoader(__DIR__.'/../templates');
 $twig = new Environment($loader, [
     'debug' => true,
 ]);
