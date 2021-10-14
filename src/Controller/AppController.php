1d9a91c82e2a8981988c2b8b24059513055e816a
cordeiro <mjcc@tutanota.com>
2021-10-14 13:37:47 +0200

    change index to reder last animals

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,11 +2,22 @@
 
 namespace App\Controller;
 
+use App\DAO\AnimalManager;
+use Twig\Environment;
+
 class AppController extends Controller
 {
+    private AnimalManager $animalManager;
+
+    public function __construct(Environment $twig)
+    {
+        $this->animalManager = new AnimalManager();
+        parent::__construct($twig);
+    }
+
     public function index(): string
     {
-        return $this->render('App/index.html.twig');
+        return $this->render('App/index.html.twig', ['animals' => $this->animalManager->findLast()]);
     }
 
     public function contact(): string
